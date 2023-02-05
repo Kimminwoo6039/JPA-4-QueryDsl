3d9a8f2571d3639df849ddc2e5b3dd73c05b5fee
kimminwoo6039 <qkqhemforhs1@gmail.com>
2023-02-05 11:46:44 +0900

    querydsl

---
+++ src/test/java/com/example/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,42 @@
+package com.example.querydsl;
+
+import com.example.querydsl.entity.Hello;
+import com.example.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Commit
+class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QHello qHello = new QHello(QHello.hello);
+      //  QHello qHello1 = QHello.hello; 2가지임
+
+        Hello result = queryFactory
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
+
+}
